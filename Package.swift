edbd35f1575b6516957936143d0bd01aa563d165
Dusan Juranovic <dusan.juranovic@gmail.com>
2021-08-08 13:17:05 +0200

    dynamic

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "StepperView",
+			type: .dynamic,
             targets: ["StepperView"]),
     ],
     dependencies: [
