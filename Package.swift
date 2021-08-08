4f437adddc13279ab89841f5c59fde7a30b4ca16
Dusan Juranovic <dusan.juranovic@gmail.com>
2021-08-08 13:54:35 +0200

    make static lib

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "StepperView",
-			type: .dynamic,
+			type: .static,
             targets: ["StepperView"]),
     ],
     dependencies: [
